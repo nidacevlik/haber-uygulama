4819c60dee1633d1b2c15cee20872bcdaa520c67
Nida Çevlik <95691553+nidacevlik@users.noreply.github.com>
2024-03-31 15:07:39 +0300

    Admin giriş sayfası yapıldı

---
+++ Proje/haber-app/app/screens/AdminLogInScreen.js
@@ -0,0 +1,60 @@
+import { Image,  StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native'
+import React from 'react'
+
+export default function WelcomeScreen() {
+  return (
+    <View className='flex-row justify-center' style={styles.container}>
+   <Image
+   resizeMode='contain'
+   style={{width:350,height:350}}
+   source={require('../assets/logo.png')}>
+   </Image> 
+   <TextInput
+        style={styles.input}
+        placeholder="E-mail"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Şifre"
+        secureTextEntry
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {}}
+      >
+        <Text style={styles.buttonText}>Giriş Yap</Text>
+      </TouchableOpacity>
+   </View>
+
+  )
+}
+ 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius:10,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  button:{
+    backgroundColor:'#ff0000',
+    borderRadius:10,
+    height:40,
+    justifyContent:'center',
+    alignItems:'center',
+
+  }
+});
+
+   
